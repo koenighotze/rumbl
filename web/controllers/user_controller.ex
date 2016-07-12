cb1d9a13f0b2f014c68c71cb91e0674e62ecb314
David Schmitz <david.schmitz.privat@gmail.com>
2016-07-12 10:05:27 +0200

    Working on auth plug

--- web/controllers/user_controller.ex
+++ web/controllers/user_controller.ex
@@ -26,7 +26,7 @@ defmodule Rumbl.UserController do
   end
 
   def create(conn, %{"user" => user_params}) do
-    changeset = User.changeset(%User{}, user_params)
+    changeset = User.registration_changeset(%User{}, user_params)
 
     case Repo.insert(changeset) do
       {:ok, user} ->
@@ -35,5 +35,9 @@ defmodule Rumbl.UserController do
            |> redirect(to: user_path(conn, :index))
       {:error, changeset} -> render(conn, "new.html", changeset: changeset)
     end
+  end
+
+  defp authenticate(conn) do
+    
   end
 end
