08f932cec2a227de3c7bd34e3484b34f5a918e4f
David Schmitz <david.schmitz.privat@gmail.com>
2016-07-18 18:26:45 +0200

    Finished chapter 6

--- web/controllers/beard_controller.ex
+++ web/controllers/beard_controller.ex
@@ -5,18 +5,33 @@ defmodule Rumbl.BeardController do
 
   plug :scrub_params, "beard" when action in [:create, :update]
 
-  def index(conn, _params) do
-    beards = Repo.all(Beard)
+
+  def action(conn, _) do
+    apply(__MODULE__, action_name(conn), [conn, conn.params, conn.assigns.current_user])
+  end
+
+  def user_beards(user) do
+    assoc(user, :beards)
+  end
+
+  def index(conn, _params, user) do
+    beards = Repo.all(user_beards(user))
     render(conn, "index.html", beards: beards)
   end
 
-  def new(conn, _params) do
-    changeset = Beard.changeset(%Beard{})
+  def new(conn, _params, user) do
+    changeset =
+        user
+        |> build_assoc(:beards)
+        |> Beard.changeset
+
     render(conn, "new.html", changeset: changeset)
   end
 
-  def create(conn, %{"beard" => beard_params}) do
-    changeset = Beard.changeset(%Beard{}, beard_params)
+  def create(conn, %{"beard" => beard_params}, user) do
+    changeset =   user
+                  |> build_assoc(:beards)
+                  |> Beard.changeset(beard_params)
 
     case Repo.insert(changeset) do
       {:ok, _beard} ->
@@ -28,19 +43,19 @@ defmodule Rumbl.BeardController do
     end
   end
 
-  def show(conn, %{"id" => id}) do
-    beard = Repo.get!(Beard, id)
+  def show(conn, %{"id" => id}, user) do
+    beard = Repo.get!(user_beards(user), id)
     render(conn, "show.html", beard: beard)
   end
 
-  def edit(conn, %{"id" => id}) do
-    beard = Repo.get!(Beard, id)
+  def edit(conn, %{"id" => id}, user) do
+    beard = Repo.get!(user_beards(user), id)
     changeset = Beard.changeset(beard)
     render(conn, "edit.html", beard: beard, changeset: changeset)
   end
 
-  def update(conn, %{"id" => id, "beard" => beard_params}) do
-    beard = Repo.get!(Beard, id)
+  def update(conn, %{"id" => id, "beard" => beard_params}, user) do
+    beard = Repo.get!(user_beards(user), id)
     changeset = Beard.changeset(beard, beard_params)
 
     case Repo.update(changeset) do
@@ -53,8 +68,8 @@ defmodule Rumbl.BeardController do
     end
   end
 
-  def delete(conn, %{"id" => id}) do
-    beard = Repo.get!(Beard, id)
+  def delete(conn, %{"id" => id}, user) do
+    beard = Repo.get!(user_beards(user), id)
 
     # Here we use delete! (with a bang) because we expect
     # it to always work (and if it does not, it will raise).
