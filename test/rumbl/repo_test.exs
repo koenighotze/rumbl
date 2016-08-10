a81ee1cbe96ea8fdd7b1d057b7f0a79bdb1e88ee
David Schmitz <david.schmitz.privat@gmail.com>
2016-08-10 08:42:31 +0200

    Fixed setup of repo test

--- test/rumbl/repo_test.exs
+++ test/rumbl/repo_test.exs
@@ -1,38 +1,43 @@
 defmodule Rumbl.RepoTest do
   use ExUnit.Case
 
-  import Rumbl.Repo
+  import Ecto.Query, only: [from: 2]
+
+  alias Rumbl.Repo
   alias Rumbl.User
+  alias Ecto.UUID
 
   @users [
     # :id, :name, :username, :password
-    %User{id: 1, name: "David Schmitz", username: "ds", password: "geheim"},
-    %User{id: 2, name: "Bratislav Metulski", username: "bm", password: "secret"},
-    %User{id: 3, name: "Baguette Salami", username: "bs", password: "tressecret"}
+    %User{name: "David Schmitz", username: UUID.generate, password: "geheim"},
+    %User{name: "Bratislav Metulski", username: UUID.generate, password: "secret"},
+    %User{name: "Baguette Salami", username: UUID.generate, password: "tressecret"}
   ]
 
   setup do
     @users
-    |> Enum.each(fn user -> insert(user) end)
+    |> Enum.each(fn user -> Repo.insert(user) end)
 
     on_exit fn ->
-      delete_all(Rumbl.User)
+      users_to_delete = @users |> Enum.map(fn user -> Map.delete(user, :password) end)
+
+      Repo.delete_all(Rumbl.User, users_to_delete)
     end
 
     :ok
   end
 
-
-
   test "fetch all users" do
-    assert 0 < length(all(Rumbl.User))
+    assert 0 < length(Repo.all(Rumbl.User))
   end
 
   test "get by id" do
-    assert get(Rumbl.User, "1").name == "David Schmitz"
+    # any_user = Repo.all(from u in User, where: not(is_nil(u.id))) |> Enum.take(1)
+    any_user = Repo.one(from u in User, limit: 1)
+    assert Repo.get(Rumbl.User, any_user.id).name == any_user.name
   end
 
   test "get by params" do
-    assert get_by!(Rumbl.User, username: "bm").name == "Bratislav Metulski"
+    assert Repo.get_by!(Rumbl.User, name: "David Schmitz").username != ""
   end
 end
